01fe4e1a204d1d7db87e7b7e116f480ce8b74141
MarcusPedrosa2017 <marcusvinicius.pp@gmail.com>
2020-07-23 00:24:00 -0300

    Navegacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,8 +12,21 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
+  //para navegar entre paginas iremos utilizar o NavController que neste caso ja esta injetado no construtor
+  //abaixo como parametro
   constructor(public navCtrl: NavController) {
 
+  }
+
+  //criando o metodo de login
+  public login(){
+    //vamos utilizar o NavController para ir para pagina de categorias ao fazer o login por hora
+    //para utilizar o objeto injetado e necessario usar a palavra "this"
+    //o metodo push empilha uma pagina sobre a outra
+    //this.navCtrl.push('CategoriasPage');
+
+    //trocamos para setRoot ao inves do push para que aparece o menu e nao a seta de paginas empilhadas
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
