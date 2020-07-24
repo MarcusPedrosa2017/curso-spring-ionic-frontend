b317336bc8fb5265dee113119ed181735fb302f5
MarcusPedrosa2017 <marcusvinicius.pp@gmail.com>
2020-07-24 13:42:35 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 //arquivo de definicao de configuracoes da api
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://curso-spring-ionic-mvpp.s3-sa-east-1.amazonaws.com"
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -16,6 +18,12 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  //variavel que ira receber o list de categorias
+  items: CategoriaDTO[];
+
+  //criando variavel para utilizar a url do bucket no template html
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
   constructor(
      public navCtrl: NavController,
      public navParams: NavParams,
@@ -30,8 +38,9 @@ export class CategoriasPage {
     //e caso de erro recebe outra funcao como argumento e imprime o erro
     this.categoriaService.findAll()
         .subscribe(response => {
-          console.log(response);
+          this.items = response;
         },
+        //funcao de erro
         error => {
           console.log(error);
         });
